28171d2252b5b0594120ef9810aa640bc920f7a8
czhc <cedric@coingecko.com>
2024-05-02 23:15:37 +0800

    Initialize account

--- NOTES.md
+++ NOTES.md
@@ -3,3 +3,8 @@
 ## Resources
 
 * [Anchor](https://www.anchor-lang.com/) - Solana's Sealevel runtime framework
+* [Program Derived Addressses (PDA)](https://solanacookbook.com/core-concepts/pdas.html): With PDAs, programs can programmatically sign for certain addresses without needing a private key. 
+    * PDAs serve as the foundation for Cross-Program Invocation, which allows Solana apps to be composable with one another.
+    * PDAs are 32 byte strings that look like public keys, but don’t have corresponding private keys
+
+#
--- anchor/programs/solana-journal/src/lib.rs
+++ anchor/programs/solana-journal/src/lib.rs
@@ -8,63 +8,45 @@ declare_id!("xNT9fpMyDBXvCMAkTafwWdQHkjrAR3gCdXjtubMSA14");
 pub mod solana_journal {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseSolanaJournal>) -> Result<()> {
-    Ok(())
-  }
+    pub fn create_entry(
+      ctx: Context<CreateEntry>,
+      title: String, 
+      message: String,
+    ) ->  Result<()> {
+      let journal_entry = &mut ctx.accounts.journal_entry;
+      journal_entry.owner = ctx.accounts.owner.key();
+      journal_entry.title = title;
+      journal_entry.message = message;
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.solana_journal.count = ctx.accounts.solana_journal.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+      Ok(())
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.solana_journal.count = ctx.accounts.solana_journal.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeSolanaJournal>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.solana_journal.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeSolanaJournal<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + SolanaJournal::INIT_SPACE,
-  payer = payer
-  )]
-  pub solana_journal: Account<'info, SolanaJournal>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseSolanaJournal<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub solana_journal: Account<'info, SolanaJournal>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub solana_journal: Account<'info, SolanaJournal>,
+    
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct SolanaJournal {
-  count: u8,
+pub struct JournalEntryState {
+  pub owner: Pubkey,
+  #[max_len(20)]
+  pub title: String,
+  #[max_len(200)]
+  pub message: String,
+  pub entry_id: u64,
+}
+
+#[derive(Accounts)] // deserialize and validate accounts
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+  #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    payer = owner,
+    space = 8 + JournalEntryState::INIT_SPACE,
+  )]
+  pub journal_entry: Account<'info, JournalEntryState>,
+  #[account(mut)]
+  pub owner: Signer<'info>,
+  pub system_program: Program<'info, System>,
 }
